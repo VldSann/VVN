627c2040d183273cf7c2d53533394cab20ea20b5
Nikorambus <72225007+Nikorambus@users.noreply.github.com>
2023-03-02 21:23:50 +0300

    Update test_api.py

--- web_site/test_api.py
+++ web_site/test_api.py
@@ -1,23 +1,27 @@
-import pytest
-from app import client
+import unittest
+import requests
 
 
-def test_index():
-    response = client.get('/news')
-    assert response.status_code == 200
-    
-def test_login():
-    response = client.get('/log')
-    assert response.status_code == 200
+class TestAPI(unittest.TestCase):
 
-def test_valid_auth():
-    data={"login": "vvn",
-          "password" : "user"}
-    response = client.post('/log', data=data)
-    assert response.status_code == 302
+    def test1(self):
+        response = requests.get('http://127.0.0.1:8000/news')
+        self.assertEqual(response.status_code, 200)
+        print("test1: OK")
 
-def test_invalid_auth():
-    data={"login": "vvn",
-          "password" : "cisco"}
-    response = client.post('/auth/login', data=data)
-    assert response.status_code == 200
+    def test2(self):
+        response = requests.get('http://127.0.0.1:8000/about')
+        self.assertEqual(response.status_code, 200)
+        print("test2: OK")
+
+    def test3(self):
+        response = requests.get('http://127.0.0.1:8000/contacts')
+        self.assertEqual(response.status_code, 200)
+        print("test3: OK")
+
+
+if __name__ == '__main__':
+    tester = TestAPI()
+    tester.test1()
+    tester.test2()
+    tester.test3()
